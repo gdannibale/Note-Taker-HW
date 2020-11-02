1ba8ab7209f2a3904ad5b9b37428136a38fe2b8b
Gina Dannibale <ginadannibale@gmail.com>
2020-11-02 13:36:48 -0600

    HW

---
+++ Develop/htmlRoutes.js
@@ -0,0 +1,57 @@
+
+
+var path = require("path");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function(req, res) {
+    res.JSON(path.join(__dirname, "./notes.html"));
+    console.log('Received a GET HTTP method');
+  });
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (db.json.length < 5) {
+      db.json.push(req.body);
+      res.json(true);
+    }
+    else {
+    }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    db.json.length = 0;
+
+    res.json({ ok: true });
+  });
+};
